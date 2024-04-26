1203d64e157d719b48e3579204ef831872964131
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-04-26 18:27:41 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
+import { faHouse } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+
+export default function Home() {
+  return (
+    <div className="">
+      nuh dduh
+      <FontAwesomeIcon className="text-red-800 text-9xl"icon={faHouse} />
+    </div>
+  );
+}
