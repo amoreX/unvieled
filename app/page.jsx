f063aea3e1592b77bb35e57e1865e70518ce51c4
WahidX <wahidx93@gmail.com>
2024-04-26 19:03:03 +0530

    added-text-signup

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,38 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faHouse } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+"use client";
 
+import { motion } from "framer-motion";
 export default function Home() {
-  return (
-    <div className="">
-      nuh dduh
-      <FontAwesomeIcon className="text-red-800 text-9xl"icon={faHouse} />
-    </div>
-  );
+	let header = "Unvieled!";
+	header = header.split("");
+	return (
+		<div
+			id="main-container"
+			className="text-white w-screen h-screen flex flex-col justify-center items-center"
+		>
+			<div id="header" className="flex flex-row text-4xl">
+				{header.map((letter, index) => {
+					return (
+						<motion.div
+							key={index}
+							initial={{ scale: 0.1, opacity: 0 }}
+							transition={{ type: "tween", delay: index * 0.1 }}
+							animate={{ scale: 1, opacity: 1 }}
+						>
+							{letter}
+						</motion.div>
+					);
+				})}
+			</div>
+
+			<motion.div
+				id="about"
+				initial={{ y: 20, opacity: 0 }}
+				transition={{ type: "tween", delay: 1 }}
+				animate={{ y: 0, opacity: 1 }}
+			>
+				Get Social
+			</motion.div>
+			<div id="signin-btn">fuck yourself</div>
+		</div>
+	);
 }
