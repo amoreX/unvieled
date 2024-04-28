b3d62ca6a7dfac11ee782616fc4940e245a94867
WahidX <wahidx93@gmail.com>
2024-04-28 16:21:10 +0530

    nav-done

--- app/Pages/Navbar/Navbar.jsx
+++ app/Pages/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
+"use client";
+import "./Navbar.scss";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { navelements } from "./svgs";
 
-import "./Navbar.scss"
-
-
-export default function Navbar(){
-    return(
-        <>
-            <div id="nav-container" >
-                huh
-            </div>
-        </>
-    )
+export default function Navbar() {
+	const [currtab, setCurrtab] = useState(0);
+	const handleclick = (ind) => {
+		setCurrtab(ind);
+	};
+	return (
+		<>
+			<motion.div
+				id="nav-container"
+				initial={{ y: 500 }}
+				animate={{ y: 0 }}
+				transition={{ type: "tween", delay: 1.5, ease: "easeOut" }}
+			>
+				{navelements.map((tab, index) => (
+					<div
+						onClick={() => {
+							handleclick(index);
+						}}
+					>
+						{index === currtab ? tab[1] : tab[0]}
+					</div>
+				))}
+			</motion.div>
+		</>
+	);
 }
