3bb99acee8253b25ce15afcaaed04b1aef62288f
WahidX <wahidx93@gmail.com>
2024-05-04 13:23:15 +0530

    folder-struct-fix

--- app/Pages/page.jsx
+++ app/Pages/page.jsx
@@ -1,14 +1,13 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import "./Home/Home.scss";
 import { motion } from "framer-motion";
 import Title from "./Home/Title";
 export default function Home() {
-  
-  return (
-    <div id="home-container">
-      <Title/>
-    </div>
-  );
+	return (
+		<div id="home-container">
+			<Title />
+		</div>
+	);
 }
